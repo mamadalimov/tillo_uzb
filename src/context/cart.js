fcd1248758edc4226b3c549472fd0066d95d60d2
mamadalimov <mamadalimovraxmatullo1@gmail.com>
2023-06-16 14:23:54 +0500

    salom

--- src/context/cart.js
+++ src/context/cart.js
@@ -5,11 +5,19 @@ const initialState = {
 }
 
 
-const cartSlide=createSlice({
+const cartSlide = createSlice({
     name: "cart",
     initialState,
     reducers:{
-
+       addToCart:( state, action )=>{
+        let index = state.value.findIndex(i => i.id === action.payload.id)
+        if (index < 0) {
+            state.value = [...state.value, {...action.payload,quantity: 1}]
+        }else{
+            state.value.map((item, inx)=> inx === index? {...action.payload,quantity: 1}:item )
+        }
+       },
+       removeFromCart: ( state, action )=>{}
     }
 })
 
