acb04214f260e271829ed4fbe896fd0353cc52b1
mamadalimov <mamadalimovraxmatullo1@gmail.com>
2023-06-16 14:13:01 +0500

    slom

---
+++ src/context/cart.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    value: []
+}
+
+
+const cartSlide=createSlice({
+    name: "cart",
+    initialState,
+    reducers:{
+
+    }
+})
+
+
+export const {} = cartSlide.actions
+export default cartSlide.reducer
