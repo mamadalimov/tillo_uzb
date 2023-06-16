1c56d8844eeba1d07820f5e44baa34f8f658ee2e
mamadalimov <mamadalimovraxmatullo1@gmail.com>
2023-06-16 12:13:34 +0500

    lorem ipsum

--- src/components/empty/Empty.js
+++ src/components/empty/Empty.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Empty.css";
 import { Link } from "react-router-dom";
+import rasm from '../../assets/wisg.webp'
+
 
 function Empty({ title }) {
   return (
     <div className="empty__container">
-     <img src="http://localhost:3000/static/media/hearts.bbeb9aa7c7332bb1cfd2.webp" alt="" />
+     <img src={rasm} alt="" />
       <h3>{title}da hozircha mahsulot yo'q</h3>
       <p>
         Bosh sahifadagi to’plamlardan boshlang yoki kerakli mahsulotni qidiruv
