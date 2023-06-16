d9295f8f89bf3d6ce48bdead3319b9ff69a3bb52
mamadalimov <mamadalimovraxmatullo1@gmail.com>
2023-06-16 12:09:15 +0500

    lorem ipsum

--- src/components/empty/Empty.js
+++ src/components/empty/Empty.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 function Empty({ title }) {
   return (
     <div className="empty__container">
+     <img src="http://localhost:3000/static/media/hearts.bbeb9aa7c7332bb1cfd2.webp" alt="" />
       <h3>{title}da hozircha mahsulot yo'q</h3>
       <p>
         Bosh sahifadagi to’plamlardan boshlang yoki kerakli mahsulotni qidiruv
--- src/static/index.js
+++ src/static/index.js
@@ -31,4 +31,34 @@ export const PRODUCTS = [
         price: 1_800_000,
         url: "https://images.uzum.uz/ci1ffp36edfostigiqn0/original.jpg"
     },
+    {
+        id: "pro-6",
+        title: "Simsiz sensorli quloqchinlar TWS i14, i15, i16, i18, i12, i11 mikrofon va keys bilan",
+        price: 33_000 ,
+        url: "https://images.uzum.uz/chb2m916i6dim8r70h10/t_product_540_high.jpg#1686730872661"
+    },
+    {
+        id: "pro-7",
+        title: "Shakar Chempion, 900 g",
+        price: 12_000 ,
+        url: "https://images.uzum.uz/cg2pqevg49devoa9ru00/original.jpg"
+    },
+    {
+        id: "pro-8",
+        title: "Smartfon Xiaomi Redmi 10 2022 4/64 GB + Powerbank Redmi, 10000 mA/soat",
+        price: 1_779_000 ,
+        url: "https://images.uzum.uz/cht747v5d7kom1tgv0t0/original.jpg"
+    },
+    {
+        id: "pro-9",
+        title: "Makaron mahsulotlari Bottega, spirallar, 400 g",
+        price: 6_000 ,
+        url: "https://images.uzum.uz/ce6vungv1htd23aip2ig/original.jpg"
+    },
+    {
+        id: "pro-10",
+        title: "Ayollar krossovkalari GRN",
+        price: 225_000 ,
+        url: "https://images.uzum.uz/ci42gmrltlh4bk4litq0/original.jpg"
+    },
 ]
